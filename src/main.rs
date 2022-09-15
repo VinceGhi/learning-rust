51a2ae2a4f9c93189e38e9a87bc8b716b9949d2d
VinceG <vg@wunschloesung.com>
2022-09-15 21:58:36 +0200

    well, that doesn't work...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
-use std::io;
+use menu::{TerminalMenu, TerminalMenuItem};
+use std::io::{stdout, Write, stdin, Read};
+
+mod menu;
+
 fn main() {
-    let mut user_input: String = String::new();
-    println!("Enter some random shit:");
-    io::stdin().read_line(&mut user_input).expect("Failed to read line! Idiot...");
-    print!("Good job.. you indeed entered some random shit: {}", user_input);
+    let mut menu: TerminalMenu = TerminalMenu {title: "Test Menu".to_string(), items: Vec::new(), selection_char: '>'};
+    let mut items: Vec<TerminalMenuItem> = Vec::new();
+    items.push(TerminalMenuItem { text: "Entry one".to_string(), exit: false, menu: Option::Some(&menu) });
+    items.push(TerminalMenuItem { text: "Entry two".to_string(), exit: false, menu: Option::Some(&menu) });
+    items.push(TerminalMenuItem { text: "Entry three".to_string(), exit: false, menu: Option::None });
+    items.push(TerminalMenuItem { text: "Entry four".to_string(), exit: false, menu: Option::None });
+    items.push(TerminalMenuItem { text: "Quit".to_string(), exit: true, menu: Option::None });
+    menu.items = items;
+
+    loop {
+        match menu.select() {
+            Ok(selected_menu_item) => {
+                println!("You've selected: {}", selected_menu_item.text);
+                if selected_menu_item.exit {
+                    break;
+                }
+                pause();
+            },
+            Err(_) => {
+                break;
+            }
+        }
+    }
+}
+
+fn pause() {
+    let mut stdin = stdin();
+    let mut stdout = stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
 }
---
+++ src/menu.rs
@@ -0,0 +1,105 @@
+use core::time;
+use std::io::{stdout, Write};
+use crossterm::{cursor::MoveTo, ExecutableCommand, terminal::{ClearType, Clear}, event::{Event, self, KeyCode}};
+
+pub struct TerminalMenu<'a> {
+    pub title: String,
+    pub items: Vec<TerminalMenuItem<'a>>,
+    pub selection_char: char
+}
+
+impl TerminalMenu<'_> {
+    pub fn select(&self) -> Result<&TerminalMenuItem, String> {
+        enter();
+        let mut selection: u16 = 0;
+        self.print_with_selection(&selection);
+        loop {
+           let event_is_waiting: bool = crossterm::event::poll(time::Duration::from_millis(20)).expect("Coudln't check if a terminal event is there...");
+
+           if event_is_waiting {
+            match crossterm::event::read() {
+                Ok(event) => {
+                    match event {
+                        Event::FocusGained => {},
+                        Event::FocusLost => {},
+                        Event::Key(key_event) => {
+                            let key: KeyCode = key_event.code;
+
+                            if key == event::KeyCode::Esc {
+                                exit();
+                                return Err("User pressed ESC".to_string());
+                            } else if key == event::KeyCode::Enter || key == event::KeyCode::Char(' ') {
+                                exit();
+                                let selected_menu_item: &TerminalMenuItem = &self.items[selection as usize];
+                                if selected_menu_item.menu.is_some() {
+                                    return selected_menu_item.menu.unwrap().select();
+                                }
+                                
+                                return Ok(selected_menu_item);
+                            } else if key == event::KeyCode::Up || key == event::KeyCode::Char('w') {
+                                if selection == 0 {
+                                    selection = (self.items.len() - 1) as u16;
+                                } else {
+                                    selection -= 1;
+                                }
+                            } else if key == event::KeyCode::Down || key == event::KeyCode::Char('s') {
+                                selection += 1;
+                                if selection >= self.items.len() as u16 {
+                                    selection = 0;
+                                }
+                            }
+
+                            self.print_with_selection(&selection);
+                        },
+                        Event::Mouse(_) => {},
+                        Event::Paste(_) => {},
+                        Event::Resize(_, _) => {}
+                    }
+                },
+                Err(_) => {},
+            }
+           }
+        }
+    }
+
+    fn print_with_selection(&self, selection: &u16) {
+        move_cursor(0, 0);
+        print!("{}", self.title);
+        for i in 0..self.items.len() {
+            move_cursor(0, 1 + (i as u16));
+            if i == *selection as usize {
+                print!("{} {}", self.selection_char, self.items[i].text);
+            } else {
+                print!("  {}", self.items[i].text);
+            }
+        }
+        stdout().flush().unwrap();
+    }  
+}
+
+pub struct TerminalMenuItem<'a> {
+    pub text: String,
+    pub exit: bool,
+    pub menu: Option<&'a TerminalMenu<'a>>
+}
+
+fn move_cursor(x: u16, y: u16) {
+    stdout().execute(MoveTo(x, y)).expect("Couldn't move Cursor!");
+}
+
+fn clear_terminal() {
+    stdout().execute(Clear(ClearType::All)).expect("Couldn't clear terminal!");
+    move_cursor(0, 0);
+}
+
+fn enter() {
+    clear_terminal();
+    stdout().execute(crossterm::cursor::Hide).expect("Couldn't hide cursor");
+    crossterm::terminal::enable_raw_mode().expect("Rawmode can't be enabled?");
+}
+
+fn exit() {
+    clear_terminal();
+    stdout().execute(crossterm::cursor::Show).expect("Couldn't show cursor");
+    crossterm::terminal::disable_raw_mode().expect("Rawmode can't be disabled?");
+}
