7e7e97881cad1c36db92a4516ebdff33cc7279b5
VinceG <vg@wunschloesung.com>
2022-09-16 02:13:49 +0200

    i guess returning the function is better than calling it in the loop?

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,45 @@
-use menu::{TerminalMenu, TerminalMenuItem};
-use std::{process};
+use menu::{
+    TerminalMenu, 
+    TerminalMenuItem
+};
+use std::process;
 
 mod menu;
 
 fn main() {
     // this whole thing could somehow work i guess... but.. as someone mentioned on the official discord server for rust: i have to get rid of the java habbits :(
-    main_menu();
+    create_main_menu();
 }
 
-fn main_menu() {
+fn create_main_menu() {
     let mut menu: TerminalMenu = TerminalMenu {title: "Test Menu".to_string(), items: Vec::new(), selection_char: '>'};
     menu.items = vec![
-        TerminalMenuItem { text: "Sub Menu".to_string(), exit: false, action: Option::Some(sub_menu), back: false },
+        TerminalMenuItem { text: "Sub Menu".to_string(), exit: false, action: Option::Some(create_sub_menu), back: false },
         TerminalMenuItem { text: "Entry two".to_string(), exit: false, action: Option::None, back: false },
         TerminalMenuItem { text: "Entry three".to_string(), exit: false, action: Option::None, back: false },
         TerminalMenuItem { text: "Entry four".to_string(), exit: false, action: Option::None, back: false },
         TerminalMenuItem { text: "Quit".to_string(), exit: true, action: Option::Some(exit), back: false }
     ];
-    menu.select();
+    menu.select().expect("Well, there is no function...")();
 }
 
-fn sub_menu() {
-    let mut sub_menu: TerminalMenu = TerminalMenu {title: "Test Menu".to_string(), items: Vec::new(), selection_char: '>'};
+fn create_sub_menu() {
+    let mut sub_menu: TerminalMenu = TerminalMenu {title: "Test Sub Menu".to_string(), items: Vec::new(), selection_char: '>'};
     sub_menu.items= vec![
-        TerminalMenuItem { text: "Entry one".to_string(), exit: false, action: Option::None, back: false },
-        TerminalMenuItem { text: "Back".to_string(), exit: false, action: Option::Some(main_menu), back: true }
+        TerminalMenuItem { text: "Back".to_string(), exit: false, action: Option::Some(create_main_menu), back: true },
+        TerminalMenuItem { text: "Sub Sub Menu".to_string(), exit: false, action: Option::Some(create_sub_sub_menu), back: true },
+        TerminalMenuItem { text: "Entry one".to_string(), exit: false, action: Option::None, back: false }
     ];
-    sub_menu.select();
+    sub_menu.select().expect("Well, there is no function...")();
+}
+
+fn create_sub_sub_menu() {
+    let mut sub_menu: TerminalMenu = TerminalMenu {title: "Test Sub Sub Menu".to_string(), items: Vec::new(), selection_char: '>'};
+    sub_menu.items= vec![
+        TerminalMenuItem { text: "Back".to_string(), exit: false, action: Option::Some(create_sub_menu), back: true },
+        TerminalMenuItem { text: "Entry one".to_string(), exit: false, action: Option::None, back: false }
+    ];
+    sub_menu.select().expect("Well, there is no function...")();
 }
 
 fn exit() {
--- src/menu.rs
+++ src/menu.rs
@@ -1,66 +1,66 @@
-use core::time;
+use crossterm::{
+    cursor::MoveTo,
+    event::{self, Event, KeyCode},
+    terminal::{Clear, ClearType},
+    ExecutableCommand,
+};
 use std::io::{stdout, Write};
-use crossterm::{cursor::MoveTo, ExecutableCommand, terminal::{ClearType, Clear}, event::{Event, self, KeyCode}};
 
 pub struct TerminalMenu {
     pub title: String,
     pub items: Vec<TerminalMenuItem>,
-    pub selection_char: char
+    pub selection_char: char,
 }
 
 impl TerminalMenu {
-    pub fn select(&self) {
+    pub fn select(&self) -> Option<fn()> {
         enter();
         let mut selection: u16 = 0;
         self.print_with_selection(&selection);
         loop {
-           let event_is_waiting: bool = crossterm::event::poll(time::Duration::from_millis(20)).expect("Coudln't check if a terminal event is there...");
-
-           if event_is_waiting {
             match crossterm::event::read() {
-                Ok(event) => {
-                    match event {
-                        Event::FocusGained => {},
-                        Event::FocusLost => {},
-                        Event::Key(key_event) => {
-                            let key: KeyCode = key_event.code;
+                Ok(event) => match event {
+                    Event::FocusGained => {}
+                    Event::FocusLost => {}
+                    Event::Key(key_event) => {
+                        let key: KeyCode = key_event.code;
 
-                            if key == event::KeyCode::Esc {
-                                exit();
-                                break;
-                            } else if key == event::KeyCode::Enter || key == event::KeyCode::Char(' ') {
-                                let selected_menu_item: &TerminalMenuItem = &self.items[selection as usize];
-                                match selected_menu_item.action {
-                                    Some(action) => {
-                                        exit();
-                                        action();
-                                        break;
-                                    },
-                                    None => {},
+                        if key == event::KeyCode::Esc {
+                            exit();
+                            return Option::None;
+                        } else if key == event::KeyCode::Enter || key == event::KeyCode::Char(' ') {
+                            let selected_menu_item: &TerminalMenuItem =
+                                &self.items[selection as usize];
+                            exit();
+                            match selected_menu_item.action {
+                                Some(action) => {
+                                    return Option::Some(action);
                                 }
-                            } else if key == event::KeyCode::Up || key == event::KeyCode::Char('w') {
-                                if selection == 0 {
-                                    selection = (self.items.len() - 1) as u16;
-                                } else {
-                                    selection -= 1;
-                                }
-                            } else if key == event::KeyCode::Down || key == event::KeyCode::Char('s') {
-                                selection += 1;
-                                if selection >= self.items.len() as u16 {
-                                    selection = 0;
+                                None => {
+                                    return Option::None;
                                 }
                             }
+                        } else if key == event::KeyCode::Up || key == event::KeyCode::Char('w') {
+                            if selection == 0 {
+                                selection = (self.items.len() - 1) as u16;
+                            } else {
+                                selection -= 1;
+                            }
+                        } else if key == event::KeyCode::Down || key == event::KeyCode::Char('s') {
+                            selection += 1;
+                            if selection >= self.items.len() as u16 {
+                                selection = 0;
+                            }
+                        }
 
-                            self.print_with_selection(&selection);
-                        },
-                        Event::Mouse(_) => {},
-                        Event::Paste(_) => {},
-                        Event::Resize(_, _) => {}
+                        self.print_with_selection(&selection);
                     }
+                    Event::Mouse(_) => {}
+                    Event::Paste(_) => {}
+                    Event::Resize(_, _) => {}
                 },
-                Err(_) => {},
+                Err(_) => {}
             }
-           }
         }
     }
 
@@ -76,33 +76,41 @@ impl TerminalMenu {
             }
         }
         stdout().flush().unwrap();
-    }  
+    }
 }
 
 pub struct TerminalMenuItem {
     pub text: String,
     pub exit: bool,
     pub back: bool,
-    pub action: Option<fn()>
+    pub action: Option<fn()>,
 }
 
 fn move_cursor(x: u16, y: u16) {
-    stdout().execute(MoveTo(x, y)).expect("Couldn't move Cursor!");
+    stdout()
+        .execute(MoveTo(x, y))
+        .expect("Couldn't move Cursor!");
 }
 
 fn clear_terminal() {
-    stdout().execute(Clear(ClearType::All)).expect("Couldn't clear terminal!");
+    stdout()
+        .execute(Clear(ClearType::All))
+        .expect("Couldn't clear terminal!");
     move_cursor(0, 0);
 }
 
 fn enter() {
-    clear_terminal();
-    stdout().execute(crossterm::cursor::Hide).expect("Couldn't hide cursor");
     crossterm::terminal::enable_raw_mode().expect("Rawmode can't be enabled?");
+    stdout()
+        .execute(crossterm::cursor::Hide)
+        .expect("Couldn't hide cursor");
+    clear_terminal();
 }
 
 fn exit() {
-    clear_terminal();
-    stdout().execute(crossterm::cursor::Show).expect("Couldn't show cursor");
     crossterm::terminal::disable_raw_mode().expect("Rawmode can't be disabled?");
+    stdout()
+        .execute(crossterm::cursor::Show)
+        .expect("Couldn't show cursor");
+    clear_terminal();
 }
